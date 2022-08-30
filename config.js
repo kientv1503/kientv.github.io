e8212467898a72d99c4e00edec8fe0cbf30a86ce
kientv1503 <112476604+kientv1503@users.noreply.github.com>
2022-08-30 16:56:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chengg Chengg';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100006272837126';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = '♥️Happy Birthday♥️';
+const contentCard = 'Chúc bạn Trang sinh nhật zui zẻ, luôn luôn xinh đẹp và hạnh phúc ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
